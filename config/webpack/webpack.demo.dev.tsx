d580eb3b7afbfcf6e45ebd77c08bbd0899d7ee3a
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2024-06-10 23:14:22 -0600

    Implemented webpack hot reload and remove index template to be loaded on lib components

---
+++ config/webpack/webpack.demo.dev.tsx
@@ -0,0 +1,14 @@
+const configAppDev = require("lib-components-frontend-ts/lib/webpack/webpack.config.dev");
+
+module.exports = (arg: any, env: any) => {
+
+  const mode = "development";
+  const htmlTitle = "Stock Track Dev build demo";
+  const dotEnvFile = ".env.demo.development";
+  const dirname = __dirname;
+
+  let mainWebpack = configAppDev.executeConfigDev(mode, { htmlTitle: htmlTitle, dirname: dirname, dotEnvFile: dotEnvFile });
+
+  console.log("Webpack config dev", JSON.stringify(mainWebpack, null, 2));
+  return mainWebpack;
+}
