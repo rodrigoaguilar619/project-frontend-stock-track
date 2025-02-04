81bf8134b1bed083d7ed464d4b4e61536852c433
rodrigoaguilar619 <rodrigoaguilar619@gmail.com>
2025-02-03 22:37:38 -0600

    Adjust webpack to set it into group bundlers

--- config/bundlers/webpack/webpack.dev.server.tsx
+++ config/bundlers/webpack/webpack.dev.server.tsx
@@ -1,4 +1,4 @@
-const configAppDevServer = require("lib-components-react/lib/webpack/webpack.config.server");
+const configAppDevServer = require("lib-components-react/lib/bundlers/webpack/webpack.config.server");
 
 module.exports = (arg: any, env: any) => {
 
